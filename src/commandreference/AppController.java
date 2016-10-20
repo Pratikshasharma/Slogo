e266c276827188586dfd599958e8095493964b80
TeddyMarchildon <tmm61@duke.edu>
2016-10-19 22:45:37 -0400

    tried to add some more integration with access to the frontend

--- src/commandreference/AppController.java
+++ src/commandreference/AppController.java
@@ -1,10 +1,30 @@
 package commandreference;
 
 import Simulation.SimulationController;
+import gui.GUIController;
 
 public class AppController {
 	
-	SimulationController mySimulationController = new SimulationController();
-	DisplayController myDisplayController = new DisplayController();
+	SimulationController mySimulationController;
+	GUIController myGUIController;
 	
+	public AppController(GUIController guiController, SimulationController simulationController){
+		mySimulationController = simulationController;
+		myGUIController = guiController;
+	}
+	
+	public void work(){
+		sendCommand();
+		updatePositions();
+	}
+	
+	private void sendCommand(){
+		//TODO: implement send to back
+		//String command = myGUIController.getCommandEntered();
+	}
+	
+	private void updatePositions(){
+		//Get updated values from backend
+		//myGUIController.updateLocation(x, y);
+	}
 }
--- src/gui/GUIController.java
+++ src/gui/GUIController.java
@@ -1,20 +1,28 @@
 package gui;
 
-import java.net.MalformedURLException;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 
 public class GUIController {
+	
     private MainGUI myMainGUI;
     public static final double SCENE_WIDTH = 900;
     public static final double SCENE_HEIGHT = 650;
     
-    public GUIController() throws MalformedURLException{
+    public GUIController() {
         myMainGUI = new MainGUI();
     }
     
     public Scene init(){
         Scene myScene = new Scene(myMainGUI.createRoot(),SCENE_WIDTH,SCENE_HEIGHT,Color.WHITE);
         return myScene;
+    }
+    
+    public String getCommandEntered(){
+    	return myMainGUI.getConsole().getTextField().getText();
+    }
+    
+    public void updateLocation(double x, double y){
+    	myMainGUI.getTurtle().setPosition(x, y);
     }
 }
