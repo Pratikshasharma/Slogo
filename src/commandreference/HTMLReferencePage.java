12afd7d5c27db779710f48a2f5e7a3d6844a7dd4
TeddyMarchildon <tmm61@duke.edu>
2016-10-26 22:54:51 -0400

    Added the html for the help page

--- src/commandreference/HTMLReferencePage.java
+++ src/commandreference/HTMLReferencePage.java
@@ -1,23 +1,34 @@
 package commandreference;
 
 import java.awt.Desktop;
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.nio.file.Files;
+import java.util.Scanner;
 
 public class HTMLReferencePage {
 
 	public void getPage(){
 		//		try {
-		//			/* The following gets the HTML content
-		//			 * 
-		//			 */
-		//			String a = "http://www.cs.duke.edu/courses/compsci308/fall16/assign/03_slogo/commands.php";
+		/* The following gets the HTML content
+		 * 
+		 */
+		File file;
+		try {
+			file = new File("src/commandreference/commandReference.html");
+			StringBuilder html = new StringBuilder();
+			Scanner in = new Scanner(file);
+			while(in.hasNextLine()){
+				html.append(in.nextLine());
+			}
+			in.close();
+			Files.write(file.toPath(), html.toString().getBytes());
+		    Desktop.getDesktop().browse(file.toURI());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		//			String a = "http://www.cs.duke.edu/courses/compsci308/fall16/assign/03_slogo/commands2_J2W.php";
 		//			URL url = new URL(a);
 		//			URLConnection conn = url.openConnection();
 		//
@@ -30,19 +41,21 @@ public class HTMLReferencePage {
 		//				System.out.println(inputLine);
 		//			}
 		//			br.close();
+		//		}
 
 		/*
 		 * This part opens a particular URL in the computer's default browser
 		 */
-		Desktop d = Desktop.getDesktop();
-		try {
-			d.browse(new URI("http://www.cs.duke.edu/courses/compsci308/fall16/assign/03_slogo/commands.php"));
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		//			Desktop d = Desktop.getDesktop();
+		//			try {
+		//				d.browse(new URI("http://www.cs.duke.edu/courses/compsci308/fall16/assign/03_slogo/commands.php"));
+		//		catch (URISyntaxException e) {
+		//			e.printStackTrace();
+		//		} 
+		//		catch (MalformedURLException e) {
+		//			e.printStackTrace();
+		//		} catch (IOException e) {
+		//			e.printStackTrace();
+		//		}
 	}
 }
