3be4f7f8cdb153b3843ecc4838513649515eaa6e
Vincent Zhang <vwz2@duke.edu>
2016-10-29 01:04:43 -0400

    fixed local variable checking for most part

--- src/ComplexCommands/For.java
+++ src/ComplexCommands/For.java
@@ -22,7 +22,13 @@ public class For extends ComplexCommand{
             myCommandStorage.addVariable(variable, (double) i);
             result=myCommandProcess.executeList(myCommandStorage, args.get(4));
         }
-        myCommandStorage.setVariableMap(originalvariables);
+        Map<String,Double> tempmap= new HashMap<String,Double>(myCommandStorage.getVariableMap());
+        Map<String,Double> updatedvariables= new HashMap<String,Double>();
+        for(String var:originalvariables.keySet()){
+            updatedvariables.put(var,tempmap.get(var));
+        }
+        myCommandStorage.setVariableMap(updatedvariables);
+        
         return result;    
     }
 }
