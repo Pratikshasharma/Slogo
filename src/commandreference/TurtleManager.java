2d96153f8d3f4ee7ffcaa55181c3a671df53daf0
TeddyMarchildon <tmm61@duke.edu>
2016-11-01 10:54:33 -0400

    Removed comments and an unnecessary instance variable

--- src/commandreference/TurtleManager.java
+++ src/commandreference/TurtleManager.java
@@ -4,53 +4,33 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-//import gui.FrontTurtle;
-
 public class TurtleManager {
 	
-//	Map<Integer, FrontTurtle> myTurtles;
-	Map<Integer, Turtleable> myNewTurtles;
-	private Turtleable myActiveTurtle;
+	Map<Integer, Turtleable> myTurtles;
 	private int myActiveID;
 	
 	public TurtleManager(){
-//		myTurtles = new HashMap<Integer, FrontTurtle>();
-		myNewTurtles = new HashMap<Integer, Turtleable>();
+		myTurtles = new HashMap<Integer, Turtleable>();
 	}
 	
 	public void addTurtle(int id, Turtleable turtle){
-//		myTurtles.put(id, new FrontTurtle(id, turtle));
-		myNewTurtles.put(id, turtle);
+		myTurtles.put(id, turtle);
 	}
 	
-//	public Collection<FrontTurtle> getTurtles(){
-//		return myTurtles.values();
-//	}
-	
-	public Collection<Turtleable> getNewTurtles(){
-		return myNewTurtles.values();
+	public Collection<Turtleable> getTurtles(){
+		return myTurtles.values();
 	}
-	
-//	public FrontTurtle getTurtleAtIndex(int index){
-//		return myTurtles.get(index);
-//	}
 	
 	public Turtleable getTurtleAtIndex(int index){
-		return myNewTurtles.get(index);
+		return myTurtles.get(index);
 	}
 	
 	public void setActiveTurtle(int id){
-//		myActiveTurtle = myTurtles.get(id);
-		myActiveTurtle = myNewTurtles.get(id);
 		myActiveID = id;
 	}
-	
-//	public FrontTurtle getActiveTurtle(){
-//		return myActiveTurtle;
-//	}
-	
+
 	public Turtleable getActiveTurtle(){
-		return myActiveTurtle;
+		return myTurtles.get(myActiveID);
 	}
 	
 	public int getActiveID(){
