4e8b521b7427f8aebe2d3308c7c7045f752555b0
TeddyMarchildon <tmm61@duke.edu>
2016-10-31 20:39:48 -0400

    added resources for new buttons

--- src/commandreference/AppController.java
+++ src/commandreference/AppController.java
@@ -11,10 +11,8 @@ import gui.GUIController;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.MapChangeListener;
-import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
