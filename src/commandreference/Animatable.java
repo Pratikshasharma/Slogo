73b1a7ec59af806048cda1272d9723ea0d710926
TeddyMarchildon <tmm61@duke.edu>
2016-11-07 21:44:18 -0500

    Added the masterpiece files

--- src/commandreference/Animatable.java
+++
@@ -1,8 +0,0 @@
-//package commandreference;
-//
-//import gui.FrontTurtle;
-//
-//public interface Animatable {
-//	
-//	public void setStepMethod(FrontTurtle turtle);
-//}
--- src/gui/HistoryClickable.java
+++ src/gui/HistoryClickable.java
@@ -1,3 +1,6 @@
+// This entire file is part of my masterpiece.
+// Ted Marchildon
+
 package gui;
 
 import javafx.scene.control.ListView;
